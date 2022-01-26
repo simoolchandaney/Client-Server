2bf5e9d866502baa08a9e0355d0312997d1c4a76
Jacob Schwartz <jschwar5@nd.edu>
2022-01-25 21:53:43 -0500

    server start

--- server/server.c
+++ server/server.c
@@ -15,7 +15,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-#define PORT "3490"  // the port users will be connecting to
+#define PORT "41111"  // the port users will be connecting to
 
 #define BACKLOG 10   // how many pending connections queue will hold
 
@@ -40,7 +40,7 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[])
+int main(void)
 {
     int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
     struct addrinfo hints, *servinfo, *p;
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE; // use my IP
 
-    if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
@@ -114,6 +114,13 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        int n;
+        char filename[BUFSIZ];
+
+        n = recv(new_fd, filename, BUFSIZ, 0);
+        if (n <= 0) {
+            perror("recv");
+        }
         inet_ntop(their_addr.ss_family,
             get_in_addr((struct sockaddr *)&their_addr),
             s, sizeof s);
@@ -121,10 +128,16 @@ int main(int argc, char *argv[])
 
         if (!fork()) { // this is the child process
             close(sockfd); // child doesn't need the listener
-            if (send(new_fd, "Hello, world!", 13, 0) == -1)
-                perror("send");
-            close(new_fd);
-            exit(0);
+
+            char data[BUFSIZ] = {0};
+            FILE *fp = fopen(filename, "r");
+
+            while(fgets(data, BUFSIZ, fp) != NULL) {
+                if (send(new_fd, data, sizeof(data), 0) == -1)
+                    perror("send");
+                close(new_fd);
+                exit(0);
+            }
         }
         close(new_fd);  // parent doesn't need this
     }
